eb53962bdec4c725be7317c7c367bc734cbed62a
Carl Brunius <carl.brunius@chalmers.se>
2018-04-25 09:51:27 +0200

    Updated preProcess()

--- R/preProcess.R
+++ R/preProcess.R
@@ -3,13 +3,13 @@
 #' @param X Data matrix with samples in rows and variables in columns
 #' @param offset Add offset to all data points (defaults to 0)
 #' @param zeroOffset Add offset to zero data (defaults to 0)
-#' @param trans If specified, accomodates 'log' or 'sqrt' transforms (default is no transformation)
-#' @param center Either a logical value or a numeric vector of length equal to the number of columns of X (defaults to TRUE for mean centering).
-#' @param scale Either 'UV', 'Pareto', 'none' or a numeric vector of length equal to the number of columns of X (defaults to scaling to unit variance 'UV').
+#' @param trans Either 'log', 'sqrt' or 'none' (default is 'none')
+#' @param center Either 'mean', 'none' or a numeric vector of length equal to the number of columns of X (defaults to 'none').
+#' @param scale Either 'UV', 'Pareto', 'none' or a numeric vector of length equal to the number of columns of X (defaults to 'none').
 #'
 #' @return A pre-processed data matrix
 #' @export
-preProcess=function(X,offset=0,zeroOffset=0,trans,center,scale) {
+preProcess=function(X,offset=0,zeroOffset=0,trans='none',center='none',scale='none') {
   nVar=nrow(X)
   # Add offset
   X=X+offset
@@ -17,9 +17,9 @@ preProcess=function(X,offset=0,zeroOffset=0,trans,center,scale) {
   X[X==0]=zeroOffset
   cat('\nZero offset by',zeroOffset)
   # Perform transformation
-  if(!missing(trans)) {
-    trans=match.arg(trans,c('log','sqrt'))
-    cat('\nTransformation:',trans)
+  trans=match.arg(trans,c('log','sqrt','none'))
+  cat('\nTransformation:',trans)
+  if(trans!='none') {
     if(trans=='log') {
       if(any(X<=0)) stop('no zero or negative values allowed when doing log transformation')
       X=apply(X,2,log) 
@@ -29,10 +29,12 @@ preProcess=function(X,offset=0,zeroOffset=0,trans,center,scale) {
       X=apply(X,2,sqrt) 
     }
   }
-  if (missing(center)) center=TRUE 
-  if(!(is.logical(center) | length(center)==nVar)) stop('center should be either a logical value or a numeric vector of length equal to the number of columns of x')
-  if(is.logical(center)) cat('\nCenter:',center) else cat('\nCenter: By vector')
-  if (missing(scale)) scale='UV' 
+  # Perform centering and scaling
+  if (length(center)!=nVar) {
+    center=match.arg(center,c('mean','none'))
+    cat('\nCenter:',center)
+    if (center=='mean') center=TRUE else center=FALSE
+  } else cat('\nCenter: By vector')
   if (length(scale)!=nVar) {
     scale=match.arg(scale,c('UV','Pareto','none'))
     cat('\nScale:',scale)
