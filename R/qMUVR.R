145334e673c561ea91e23d2fb11ceab2d57261e7
Carl Brunius <carl.brunius@chalmers.se>
2019-06-20 15:15:37 +0200

    Added qMUVR

---
+++ R/qMUVR.R
@@ -0,0 +1,28 @@
+#' qMUVR: Wrapper for speedy access to MUVR (autosetup of parallelization)
+#'
+#' @param X X-data
+#' @param Y Y-data
+#' @param ML Boolean for multilevel
+#' @param method 'RF' (default) or 'PLS'
+#' @param varRatio proportion of variables to keep in each loop of the recursive feature elimination
+#' @param nCore Number of threads to use for calculation (defaults to detectCores()-1)
+#' @param repMult Multiplier of cores -> nRep = repMult * nCore
+#' @param nOuter Number of outer segments
+#'
+#' @return MUVR object
+#' @export
+qMUVR <- function(X, Y, ML=F, method='RF', varRatio=0.65, nCore, repMult=1, nOuter=5) {
+  library(doParallel)
+  library(MUVR)
+  if (missing(nCore)) nCore=detectCores()-1
+  nRep <- repMult * nCore
+  cl <- makeCluster(nCore)
+  registerDoParallel(cl)
+  if (ML) {
+    mod <- MUVR(X=X, ML=T, method=method, nRep=nRep, nOuter=nOuter, varRatio = varRatio)
+  } else {
+    mod <- MUVR(X=X, Y=Y, method=method, nRep=nRep, nOuter=nOuter, varRatio = varRatio)
+  }
+  stopCluster(cl)
+  return(mod)
+}
