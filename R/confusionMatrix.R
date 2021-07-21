cadf976be7a258c63587a165127a5730caa1fb95
Yingxiao Yan <yingxiao@net.chalmers.se>
2021-07-21 16:56:55 +0200

    MUVR class Object

--- R/confusionMatrix.R
+++ R/confusionMatrix.R
@@ -1,15 +1,15 @@
 #' Make a confusion matrix from a MUVR object
 #'
-#' @param MVObj A MUVR object (classification analysis)
+#' @param MUVRclassObject A MUVR class object (classification analysis)
 #' @param model min, mid or max model
 #'
 #' @return A confusion matrix of actual vs predicted class
 #' @export
 #'
-confusionMatrix=function(MVObj,model='mid'){
-  if(!any(class(MVObj)=='Classification')) stop ('The MUVR object needs to be from a classification analysis')
+confusionMatrix=function(MUVRclassObject,model='mid'){
+  if(!any(class(MUVRclassObject)=='Classification')) stop ('The MUVR object needs to be from a classification analysis')
   nMod=ifelse(model=='min',1,ifelse(model=='mid',2,3))
-  actual=MVObj$inData$Y
-  predicted=MVObj$yClass[,nMod]
+  actual=MUVRclassObject$inData$Y
+  predicted=MUVRclassObject$yClass[,nMod]
   table(actual=actual,predicted=predicted)
 }
