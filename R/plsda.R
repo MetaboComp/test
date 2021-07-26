ade3b573a063749f5f7b295dbc69050aeab86702
Yingxiao Yan <yingxiao@net.chalmers.se>
2021-07-26 10:51:35 +0200

    I added some comments for my own understanding.

--- R/plsda.R
+++ R/plsda.R
@@ -11,27 +11,46 @@
 #' @param scale
 #'
 #' @return plsda object
-#' @export
+#'
 plsda <- function(x,
-                  y, ncomp = 2, max.iter = 500, tol = 1e-06, near.zero.var = TRUE, scale = TRUE) {
+                  y,
+                  ncomp = 2,
+                  max.iter = 500,
+                  tol = 1e-06,
+                  near.zero.var = TRUE,
+                  scale = TRUE) {
   y = as.factor(y)
 
   n <- length(y)
   groups <- sort(unique(y))
-  levels =  levels(y)### Add levels
-  cgroups <- as.character(groups)
-  groups <- as.numeric(factor(cgroups, levels = unique(cgroups)))
-  classification <- as.numeric(factor(as.character(y), levels = unique(cgroups)))
-  k <- length(groups)
-  nam <- levels(groups)
-  ind.mat <- matrix(0, n, k, dimnames = c(names(classification), nam))
-  for (j in 1:k) ind.mat[classification == groups[j], j] <- 1
-  attr(ind.mat, "levels") = levels
 
-  result = MUVR::pls(x, ind.mat, ncomp = ncomp, max.iter = max.iter, tol = tol,near.zero.var = near.zero.var, scale = scale)
+  levels =  levels(y)### Add levels
+  ###To make sure the sequence do not change here.
+  cgroups <- as.character(groups)
+  groups <- as.numeric(factor(cgroups, levels = unique(cgroups)))  ###I also used this in one hot encoding. Actually Plsda is really similar to one hot encoding
+  classification <- as.numeric(factor(as.character(y), levels = unique(cgroups)))
+
+  k <- length(groups)
+  nam <- levels(factor(cgroups, levels = unique(cgroups)))####this is the group name sequencing with alphabet order
+  ind.mat <- matrix(0, n, k, dimnames = list(1:n, nam))   ###,row is obs, column is number of groups
+   ####names for rows are number, names for colnum is group levels
+
+  for (j in 1:k) ind.mat[classification == groups[j], j] <- 1
+  attr(ind.mat, "levels") = levels   ###Get or set specific attributes of an object.
+  # create a 2 by 5 matrix
+  #x <- 1:10
+  #attr(x,"dim") <- c(2, 5)
+
+  result = MUVR::pls(x,
+                     ind.mat,
+                     ncomp = ncomp,
+                     max.iter = max.iter,
+                     tol = tol,
+                     near.zero.var = near.zero.var,
+                     scale = scale)
 
   result$ind.mat = ind.mat
   result$names$Y = levels(y)
   class(result) = "plsdaMUVR"
-  return(invisible(result))
+  return(invisible(result))   ###Return a (temporarily) invisible copy of an object.
 }
