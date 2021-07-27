84226d625993529cb46e9124f0a8bf704a857691
Yingxiao Yan <yingxiao@net.chalmers.se>
2021-07-27 10:15:27 +0200

    (1)considered when actual and h0 is not numeric (2)considered when h0 has less than 5 values for a distribution (3)considered when the input of side and type is not the input expected (4)When type="non", actual is from Q2 or AUC,actual is not rank[1]

--- R/pPerm.R
+++ R/pPerm.R
@@ -1,23 +1,46 @@
 #' Calculate permutation p-value of actual model performance vs null hypothesis distribution
-#' 
-#' `pPerm` will calculate the cumulative (1-tailed) probability of `actual` belonging to `h0`. 
+#'
+#' `pPerm` will calculate the cumulative (1-tailed) probability of `actual` belonging to `h0`.
 #' Side is guessed by actual value compared to median(h0).
 #' Test is performed on original data OR ranked for non-parametric statistics.
 #' @param actual Actual model performance (e.g. misclassifications or Q2)
 #' @param h0 Null hypothesis distribution from permutation test (same metric as `actual`)
-#' @param side Smaller or greater than (automatically guessed if omitted) (Q2 is a "greater than" test, whereas misclassifications is "smaller than")
+#' @param side Smaller or greater than (automatically guessed if omitted) (Q2 and AUC is a "greater than" test, whereas misclassifications is "smaller than")
 #' @param type Standard Student's t distribution ('t') or Student's t on rank-transformed data for nonparametric test ('non')
 #' @return p-value
 #' @export
-pPerm=function(actual,h0,side=c('smaller','greater'),type=c('t','non')) {
-  if(missing(type)) type='t'
-  if(missing(side)) side=ifelse(actual<median(h0),'smaller','greater')
+pPerm=function(actual,
+               h0,
+               side=c('smaller','greater'),
+               type=c('t','non')) {
+##########################################################################################################################
+#it needs to be take into consideration when the type od side is error
+  if(is.numeric(actual)==F)stop("actual needs to be numeric")
+  if(is.numeric(h0)=F)stop("h0 needs to be a numeric distribution")
+  if(length(h0)<5)stop("h0 has too view values to form a distribution")
+  if(!missing(type)){if(type!="t"&type!="non")stop("This type can not be implemented")}
+  if(missing(type)) type='t'     ###Student's t distribution
+  if(!missing(side)){if(side!="smaller"&side!="greater")stop("This side can not be implemented")}
+  if(missing(side)) side=ifelse(actual<median(h0),'smaller','greater')   ###This is to see which side
+
+######################################################################################################################################################################################################
+
   if (type=='non') {
-    rank=rank(c(actual,h0))
-    actual=rank[1]
-    h0=rank[-1]
+    if(side=="smaller"){
+     rank=rank(c(actual,h0))     ###the sequence of each value
+     actual=rank[1]              ###actual is not the smallest if side is greater this apply to Q2 and AUC
+     h0=rank[-1]
+    }else{rank=rank(c(actual,h0))
+          actual=rank[(length(h0)+1)]
+          h0=rank[-(length(h0)+1)]
+          }
   }
-  p=pt((actual-mean(h0))/sd(h0),df=length(h0)-1)
-  if (side=='greater') p=1-p
+###############################################################################################################################################################################################
+  p=pt((actual-mean(h0))/sd(h0),    ##### pt gives the probability before the input point
+
+       df=length(h0)-1)
+
+  if (side=='greater') p=1-p     ####this is to solve which side p value is
+
   return(p)
 }
