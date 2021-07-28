ae28a37b16dc40fba585d87fb886101c81a4caeb
Yingxiao Yan <yingxiao@net.chalmers.se>
2021-07-28 10:28:17 +0200

    ##In the MUVR class object, it is still called VIP because it is not ranked   ##only by this getVIRank,it is ranked

--- R/getVIRank.R
+++ R/getVIRank.R
@@ -8,7 +8,9 @@
 getVIRank=function(MUVRclassObject,model='mid') {
   nMod=ifelse(model=='min',1,ifelse(model=='mid',2,3))
   nVar=round(MUVRclassObject$nVar[nMod])
-  VIRanks=sort(MUVRclassObject$VIRank[,nMod])[1:nVar]       ###sequencing them and take the first few of them
+  VIRanks=sort(MUVRclassObject$VIP[,nMod])[1:nVar]       ###sequencing them and take the first few of them
+  ##In the MUVR class object, it is still called VIP because it is not ranked
+  ##only by this getVIRank,it is ranked
   VIRanks=data.frame(order=1:nVar,name=names(VIRanks),rank=VIRanks)
   VIRanks$name=as.character(VIRanks$name)
   return(VIRanks)
