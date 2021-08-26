01c857358f468e5144b0a6556548ecc5e7b5f7f1
Yingxiao Yan <yingxiao@net.chalmers.se>
2021-08-26 10:14:53 +0200

    plspredict! not predictpls

--- R/plsInner.R
+++ R/plsInner.R
@@ -40,7 +40,7 @@ plsInner=function(xTrain,
                               near.zero.var=TRUE,
                               scale=scale)
 ####
-      yValInner=predictpls(plsModIn,
+      yValInner=plspredict(plsModIn,
                         newdata=xVal,
                         onlyPred=TRUE,
                         scale=scale)$predict[,,]    ##observation new, y variables col, component 1
