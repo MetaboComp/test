1b56c8c6fc5ecb63f157ab4fc6e29b27756c7f02
Yingxiao Yan <yingxiao@net.chalmers.se>
2021-11-02 14:24:11 +0100

    added data descriptions

---
+++ R/data.R
@@ -0,0 +1,49 @@
+#' Effect matrix for the crisp multilevel tutorial
+#'
+#' @name crispEM
+#' @docType data
+#' @keywords data
+#' @usage data(crisp)
+NULL
+
+
+#' Subject identifier for the rye metabolomics regression tutorial
+#'
+#' @name IDR
+#' @docType data
+#' @keywords data
+#' @usage data(freelive)
+NULL
+
+#' Metabolomics data for the rye metabolomics regression tutorial
+#'
+#' @name XRVIP
+#' @docType data
+#' @keywords data
+#' @usage data(freelive)
+NULL
+
+#' Rye consumption for the rye metabolomics regression tutorial
+#'
+#' @name YR
+#' @docType data
+#' @keywords data
+#' @usage data(freelive)
+NULL
+
+
+#' Microbiota composition in mosquitos for the classification tutorial
+#'
+#' @name Xotu
+#' @docType data
+#' @keywords data
+#' @usage data(mosquito)
+NULL
+
+#' Village of capture of mosquitos for the classification tutorial
+#'
+#' @name Yotu
+#' @docType data
+#' @keywords data
+#' @usage data(mosquito)
+NULL
