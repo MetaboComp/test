48e425478ec0a2c90ae07707f0b7d66db6f5cffd
Carl Brunius <carlbru@chalmers.se>
2023-03-09 13:33:56 +0100

    Updated predMV (class attribute order bug)

--- R/predMV.R
+++ R/predMV.R
@@ -8,77 +8,175 @@
 #'
 #' @return A pdf with plots of results from multivariate predictions
 #' @export
-predMV=function(MVObj,newdata,model='mid') {
-  if (!any(class(MVObj)=='MVObject')) {
+predMV <- function(MVObj,newdata,model='mid') {
+  
+  # Check for correct class of object
+  if (!any(class(MVObj) == 'MVObject')) {
     cat('\nWrong object class: Return NULL')
     return(NULL)
   }
-  modNum=ifelse(model=='min',1,ifelse(model=='mid',2,3))
-  method=MVObj$inData$method
-  nRep=MVObj$inData$nRep
-  nOuter=MVObj$inData$nOuter
-  if (method=='PLS') {
-    nComps=MVObj$nCompPerSeg[[modNum]]
+  
+  # Extract model parameters
+  modNum <- ifelse(model == 'min',
+                   1,
+                   ifelse(model == 'mid',
+                          2,
+                          3))
+  method <- MVObj$inData$method
+  nRep <- MVObj$inData$nRep
+  nOuter <- MVObj$inData$nOuter
+  
+  if (method == 'PLS') {
+    nComps <- MVObj$nCompPerSeg[[modNum]]
   } else {
     library(randomForest)
   }
-  # par(mar=c(4,4,0,0)+.5)
-  if (class(MVObj)[3]=='Regression') {
-    yPredPerMod=matrix(ncol=length(MVObj$outModels),nrow=nrow(newdata),dimnames=list(paste('observation',1:nrow(newdata),sep=''),paste('model',1:length(MVObj$outModels),sep='')))
-    n=0
+  
+  ###############
+  # Regression
+  ###############
+  
+  if (class(MVObj)[2] == 'Regression') {
+    
+    # Allocate object containing predictions per segment model 
+    yPredPerMod <- matrix(ncol = length(MVObj$outModels),
+                          nrow = nrow(newdata),
+                          dimnames = list(paste('observation', 1:nrow(newdata), sep = ''),
+                                          paste('model', 1:length(MVObj$outModels), sep = '')))
+    
+    # Make predictions for each segment
+    n <- 0 # Set segment model counter to zero
+    
     for(r in 1:nRep) {
+      
       for(i in 1:nOuter) {
-        n=n+1
-        mod=MVObj$outModels[[n]][[modNum]]
-        if (method=='PLS') {
-          if (any(!colnames(mod$X)%in%colnames(newdata))) {
-            cat('\nMismatch variable names in model',n,': Return NULL')
+        
+        n <- n + 1 # Increase segment model counter
+        
+        mod <- MVObj$outModels[[n]][[modNum]] # Extract model
+        
+        if (method == 'PLS') {
+          
+          # Sanity check predictor variables
+          if (any(!colnames(mod$X) %in% colnames(newdata))) {
+            cat('\nMismatch variable names in model', n, ': Return NULL')
             return(NULL)
-          } else {
-            X=subset(newdata,select=colnames(mod$X))
-            nComp=nComps[r,i]
-            yPredPerMod[,n]=predict(mod,newdata=X)$predict[,,nComp]  # 
+            
+          } else { # If ok then run
+            
+            # Extract model-specific params
+            X <- subset(newdata, select = colnames(mod$X)) # Subset variables to those in the actual model
+            nComp <- nComps[r, i] 
+            
+            # Make actual model predictions
+            yPredPerMod[, n] <- predict(mod, newdata = X)$predict[, , nComp]  # 
+            
           }
-        } else {
-          if (any(!rownames(mod$importance)%in%colnames(newdata))) {
-            cat('\nMismatch variable names in model',n,': Return NULL')
+          
+        } else { # If Random Forest
+          
+          # Sanity check predictor variables
+          if (any(!rownames(mod$importance) %in% colnames(newdata))) {
+            cat('\nMismatch variable names in model', n, ': Return NULL')
             return(NULL)
-          } else {
-            X=subset(newdata,select=mod$names$X)
-            X=subset(newdata,select=rownames(mod$importance))
-            yPredPerMod[,n]=predict(mod,newdata=X)  # 
+            
+          } else { # If ok then run
+            
+            # Extract model-specific params
+            X <- subset(newdata, select = mod$names$X)
+            X <- subset(newdata, select = rownames(mod$importance))
+            
+            # Make actual model predictions
+            yPredPerMod[, n] <- predict(mod, newdata=X)  # 
+            
           }
+          
         }
+        
       }
+      
     }
-    yPred=apply(yPredPerMod,1,mean)
-    return(list(yPred=yPred,yPredPerMod=yPredPerMod))
-  } else if (class(MVObj)[3]=='Classification') {
-    yPredPerMod=array(dim=c(nrow(newdata),length(levels(MVObj$inData$Y)),length(MVObj$outModels)),dimnames=list(paste('observation',1:nrow(newdata),sep=''),levels(MVObj$inData$Y),paste('model',1:length(MVObj$outModels),sep='')))
-    n=0
+    
+    # Average predictions over all models
+    yPred <- apply(yPredPerMod,1,mean)
+    
+    # Return
+    return(list(yPred = yPred,
+                yPredPerMod = yPredPerMod))
+    
+    
+  } else if (class(MVObj)[2]=='Classification') {
+
+    ###################
+    # Classification
+    ###################
+    
+    # Allocate object containing predictions per segment model 
+    yPredPerMod <- array(dim = c(nrow(newdata), 
+                                 length(levels(MVObj$inData$Y)), 
+                                 length(MVObj$outModels)),
+                         dimnames = list(paste('observation', 1:nrow(newdata), sep=''), 
+                                         levels(MVObj$inData$Y), 
+                                         paste('model', 1:length(MVObj$outModels), sep='')))
+    
+    # Make predictions for each segment
+    n <- 0 # Set segment model counter to zero
+    
     for(r in 1:nRep) {
+      
       for(i in 1:nOuter) {
-        n=n+1
-        mod=MVObj$outModels[[n]][[modNum]]
-        if (method=='PLS') {
+        
+        n <- n + 1 # Increase segment model counter
+        
+        mod <- MVObj$outModels[[n]][[modNum]] # Extract model
+        
+        if (method == 'PLS') {
           cat('\nNot yet implemented')
           return(NULL)
+          
         } else {
-          if (any(!rownames(mod$importance)%in%colnames(newdata))) {
+          # RF
+          # Sanity check predictor variables
+          if (any(!rownames(mod$importance) %in% colnames(newdata))) {
             cat('\nMismatch variable names in model',n,': Return NULL')
             return(NULL)
-          } else {
-            X=subset(newdata,select=rownames(mod$importance))
-            yPredPerMod[,,n]=predict(mod,newdata=X,type='vote')  # 
+            
+          } else { # If ok then run
+            
+            # Extract model-specific params
+            X <- subset(newdata, select = rownames(mod$importance))
+            
+            # Make actual model predictions
+            yPredPerMod[,,n] <- predict(mod, newdata = X, type='vote')  # 
+            
           }
+          
         }
+        
       }
+      
     }
-    yPred=apply(yPredPerMod,c(1,2),mean)
-    yClass=levels(MVObj$inData$Y)[apply(yPred,1,which.max)]
-    names(yClass)=paste('observation',1:nrow(newdata),sep='')
-    return(list(yClass=yClass,yPred=yPred,yPredPerMod=yPredPerMod))
+    
+    # Average predictions over all models
+    yPred <- apply(yPredPerMod, c(1, 2), mean)
+    
+    # Convert numeric predictions to class
+    yClass <- levels(MVObj$inData$Y)[apply(yPred, 1, which.max)]
+    names(yClass) <- paste('observation', 1:nrow(newdata), sep = '')
+    
+    # Return
+    return(list(yClass = yClass,
+                yPred = yPred,
+                yPredPerMod = yPredPerMod))
+    
   } else {
+    
+    ###################
+    # Multilevel
+    ###################
+    
     cat('\nNot yet implemented')
+    
   }
+  
 }
